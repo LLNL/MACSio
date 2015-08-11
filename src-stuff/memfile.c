88139bf4264fc761757b0f609ad17d19095932ce
miller86 <miller86@llnl.gov>
2015-08-11 16:50:46 -0700

    adding bolierplate GPL license text

---
+++ src-stuff/memfile.c
@@ -0,0 +1,247 @@
+/*
+Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+Produced at the Lawrence Livermore National Laboratory.
+Written by Mark C. Miller
+
+CODE-<R&R number> All rights reserved.
+
+This file is part of MACSio
+
+Please also read the LICENSE file at the top of the source code directory or
+folder hierarchy.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License (as published by the Free Software
+Foundation) version 2, dated June 1991.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <hdf5.h>
+#include <mpi.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define ARG_INT 1
+#define ARG_DBL 2
+#define ARG_STR 3
+
+#define FILE_TAG 54
+
+#if 0
+            case ARG_DBL:                                 \
+                ARGNM = (TYPE) strtod(&argv[i][n+1],&tmp);\
+                had_error = (ARGNM==0 && errno != 0);     \
+                break;                                    \
+
+#endif
+
+#define HANDLE_ARG(ARGNM,TYPE,HELP)                       \
+{                                                         \
+    int n = strlen(#ARGNM)+1;                             \
+    if (help)                                             \
+    {                                                     \
+        fprintf(stderr, "%s=<%s>: %s\n",                  \
+            #ARGNM, #TYPE, #HELP);                        \
+    }                                                     \
+    else if (!strncasecmp(argv[i], #ARGNM"=", n))         \
+    {                                                     \
+        int had_error = 0;                                \
+        char *tmp = 0;                                    \
+        errno = 0;                                        \
+        switch(typecode(#TYPE))                           \
+        {                                                 \
+            case ARG_INT:                                 \
+                ARGNM = (TYPE) strtol(&argv[i][n],0,10);\
+                had_error = (ARGNM==0 && errno != 0);     \
+                break;                                    \
+            case ARG_STR:                                 \
+                ARGNM = (TYPE) strdup(&argv[i][n]);     \
+                had_error = errno!=0;                     \
+                break;                                    \
+            default: had_error = 1;                       \
+        }                                                 \
+        if (had_error)                                    \
+            fprintf(stderr, "error processing argument \"%s\"" \
+                "with input \"%s\"\n", #ARGNM, argv[i]);  \
+    }                                                     \
+}
+
+static int typecode(char const *typestr)
+{
+    if (!strncasecmp(typestr,"int",3)) return ARG_INT;
+    if (!strncasecmp(typestr,"double",6)) return ARG_DBL;
+    if (!strncasecmp(typestr,"char*",5)) return ARG_STR;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    int rank = 0;
+    int size = 1;
+    char *memfs_path = 0;
+    int help = (argc == 1 || (argc == 2 && strcasestr(argv[1], "help")));
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    H5open();
+    H5Eset_auto1((H5E_auto1_t) H5Eprint1, stderr);
+
+    for (i = 0; i < argc; i++)
+    {
+        HANDLE_ARG(help, int, Print help);
+        HANDLE_ARG(memfs_path, char*, Path to a writeable dir
+            in a mem-based filesystem (e.g. tmpfs or ramfs));
+        if (help) exit(1);
+    }
+
+    /* rank 0 will serve as an "aggregator" recieving messages (whole files) from
+       other ranks to 'append' to a single file */
+    if (rank == 0)
+    {
+        int fd;
+        int max_len = 2*3*size*sizeof(double)+10*1024;
+        char filename[256];
+        void *recvbuf;
+        void *buf = malloc(max_len);
+        hid_t fid;
+
+        /* create the 'aggregated' results file */
+        fid = H5Fcreate("aggregated.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+        /* create a temp file in memfs space to serve as our recv buffer */
+        snprintf(filename, sizeof(filename), "%s/memfile-tmpbuf-000.h5", memfs_path, rank);
+        fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+        write(fd, buf, max_len); /* write garbage for now */
+
+        /* map the file into memory which will serve as our recv buffer */
+        errno = 0;
+        recvbuf = mmap(0, (size_t) max_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (errno != 0)
+        {
+            fprintf(stderr, "Encountered error \"%s\"\n", strerror(errno));
+            abort();
+        }
+        assert(recvbuf != 0);
+
+        for (i = 1; i < size; i++)
+        {
+            int real_len;
+            hid_t tmpfid, src_grpid, src_dsid, dsid, sid, tid;
+            char grpname[256];
+            hsize_t dims[3];
+            MPI_Status status;
+
+            /* blocking recieve from any of whole file contents */
+            MPI_Recv(recvbuf, max_len, MPI_CHAR, MPI_ANY_SOURCE, FILE_TAG, MPI_COMM_WORLD, &status);
+            MPI_Get_count(&status, MPI_CHAR, &real_len);
+            msync(recvbuf, real_len, MS_SYNC);
+            sync();
+printf("recieved %d bytes from rank %d\n", real_len, status.MPI_SOURCE);
+
+            /* create group in aggregator file for this processor's contributions */
+            snprintf(grpname, sizeof(grpname), "group-%03d", (int) status.MPI_SOURCE);
+            src_grpid = H5Gcreate1(fid, grpname, 0);
+
+            /* ok, should be safe to 'open' the file as an HDF5 file */
+            tmpfid = H5Fopen(filename, H5F_ACC_RDONLY, H5P_DEFAULT);
+
+            /* copy contents from this tmpfid to the aggregator file */
+            src_dsid = H5Dopen(tmpfid, "foo", H5P_DEFAULT);
+            tid = H5Dget_type(src_dsid);
+            sid = H5Dget_space(src_dsid);
+            dsid = H5Dcreate1(src_grpid, "foo", tid, sid, H5P_DEFAULT);
+            H5Dread(src_dsid, tid, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf);
+            H5Dwrite(dsid, tid, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf);
+            H5Dclose(dsid);
+            H5Dclose(src_dsid);
+            H5Tclose(tid);
+            H5Sclose(sid);
+            H5Fclose(tmpfid);
+        }
+
+        assert(munmap(recvbuf, (size_t) max_len)==0);
+        close(fd);
+        unlink(filename);
+
+    }
+    else
+    {
+        char filename[256];
+        hid_t fid, sid, dsid;
+        int ndims=3, n=2*3*rank;
+        hsize_t dims[3] = {2,3,rank};
+        hid_t tid = rank%2?H5T_NATIVE_DOUBLE:H5T_NATIVE_INT;
+        int *ibuf;
+        double *dbuf;
+        void *buf, *sendbuf;
+        int fd, nbytes;
+        struct stat stat_buf;
+
+        /* other ranks will create a file in memfs, mmap it and send it to rank 0
+           to aggregate */
+
+        /* Ordinarily, these wold be on different nodes of a parallel machine
+           and so in different (e.g. local) filesystems and no need to fiddle with filenames
+           to make sure they don't collide with other processor's filenames */
+        snprintf(filename, sizeof(filename), "%s/memfile-test-%03d.h5", memfs_path, rank);
+        dbuf = (double*) malloc(n * sizeof(double));
+        ibuf = (int*) malloc(n * sizeof(int));
+        buf = rank%2?(void*)dbuf:(void*)ibuf;
+        for (i = 0; i < n; i++)
+        {
+            dbuf[i] = ((double) 2 * i) / n;
+            ibuf[i] = i;
+        }
+
+        fid = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        sid = H5Screate_simple(ndims, dims, 0);
+        dsid = H5Dcreate1(fid, "foo", tid, sid, H5P_DEFAULT);
+        H5Dwrite(dsid, tid, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf);
+        H5Sclose(sid);
+        H5Dclose(dsid);
+        H5Fclose(fid);
+        sync();
+
+        fd = open(filename, O_RDONLY);
+        fstat(fd, &stat_buf); 
+        nbytes = (int) stat_buf.st_size;
+
+        /* mmap the file so we can MPI_Send it */
+        errno = 0;
+        sendbuf = mmap(0, (size_t) nbytes, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (errno != 0)
+        {
+            fprintf(stderr, "Encountered error \"%s\"\n", strerror(errno));
+            abort();
+        }
+        assert(sendbuf != 0);
+
+        MPI_Send(sendbuf, nbytes, MPI_CHAR, 0, FILE_TAG, MPI_COMM_WORLD);
+
+        /* remove the mmap mapping */
+        assert(munmap(sendbuf, (size_t) nbytes)==0);
+        close(fd);
+        unlink(filename);
+    }
+
+    MPI_Finalize();
+}
